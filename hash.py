d9dde2e3fa16d7e09c4216f8b4dc2ef18baa6c99
Python <Python@Admins-iMac-6.local>
2020-01-07 15:58:17 +0700

    XD

---
+++ hash.py
@@ -0,0 +1,36 @@
+import random
+namelist = ["","","","","","","","","",""]
+doit = "Y"
+def get_in_the_list(keys,val,num):
+    global namelist
+    Data = {keys:val}
+    if namelist[num] == "":
+        namelist[num]=Data
+    else:
+        count = -1
+        for i in namelist:
+            count+= 1
+            if i == "":
+                namelist[count] = Data
+            
+    return namelist
+def nathash():
+    global namelist
+    n = random.randint(0,9)
+    #while namelist[n] != "":
+        #n = random.randint(0,9)
+    return n
+while doit == "Y":
+    keys = input("Enter your name :")
+    val = input("number ID :")
+    num = nathash()
+    print(get_in_the_list(keys,val,num))
+    doit = input("want more(Y = Yes,N = No)")
+
+
+
+
+
+
+    
+
