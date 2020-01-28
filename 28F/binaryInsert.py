286f85b969798eec5427a0b39b501b5c610983e2
Python <Python@Admins-iMac-6.local>
2020-01-28 16:02:43 +0700

    udata

--- 28F/binaryInsert.py
+++ 28F/binaryInsert.py
@@ -3,7 +3,7 @@ class Node:
         self.left = None
         self.right = None
         self.data = data
-
+    
     def insert(self,data):
         if self.data:
             if data < self.data:
@@ -24,10 +24,84 @@ class Node:
         print(self.data),
         if self.right:
             self.right.PrintTree()
-
-root = Node(12)
-root.insert(6)
-root.insert(14)
-root.insert(3)
-
+    def findval(self,lkpval):
+        if lkpval < self.data:
+            if self.left is None:
+                return str(lkpval)+"Not Found"
+            return self.left.findval(lkpval)
+        elif lkpval > self.data:
+            if self.right is None:
+                return str(lkpval)+"Not Found"
+            return self.right.findval(lkpval)
+        else:
+            print(str(self.data)+" is Found")
+    def inorderTraversal(self,root):
+            res = []
+            if root:
+                res = self.inorderTraversal(root.left)
+                res.append(root.data)
+                res = res + self.inorderTraversal(root.right)
+            return res
+    def PreorderTraversal(self,root):
+        res = []
+        if root:
+            res.append(root.data)
+            res = res + self.PreorderTraversal(root.left)
+            res = res + self.PreorderTraversal(root.right)
+        return res
+    def PostorderTrraversal(self,root):
+        res = []
+        if root:
+            res = self.PostorderTrraversal(root.left)
+            res = res+self.PostorderTrraversal(root.right)
+            res.append(root.data)
+        return res
+    def deleltion(self,num):
+        if num < self.data:
+            if self.left is None:
+                return str(num)+"Not Found"
+            elif self.right.data == num:
+                self.right = None
+                return True
+            elif self.left.data == num:
+                self.left = None
+                return True
+            return self.left.deleltion(num)
+        elif num > self.data:
+            #x = self.right
+            #print(x)
+            #print("X")
+            #print(self.right.data)
+            if self.right is None:
+                return str(num)+"Not Found"
+            #else:
+                #self.right = None
+                #return True
+            elif  self.right.data == num:
+                self.right = None
+                #print(self.right.data)
+                return True
+            elif self.left.data == num:
+                self.left = None
+                return True
+            return self.right.deleltion(num)
+        else:
+            if self.left is None and self.right is None:
+                pass
+            
+    
+root = Node(100)
+root.insert(50)
+root.insert(60)
+root.insert(30)
+root.insert(150)
+root.insert(120)
+root.deleltion(120)
+print(root.inorderTraversal(root))
+#print(root.PreorderTraversal(root))
+#print(root.PostorderTrraversal(root))
+#for i in range(51):
+    #root.insert(i)
 root.PrintTree()
+#print(root.findval(56))
+
